93140e8625d28e694390f37c7f3ea098fa24b179
Andrei Ionut Damian <andrei.damian@me.com>
2025-11-02 20:39:40 +0000

    fix: update to py3.11

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,8 +5,4 @@ cd /workspaces/cpu_serving
 
 npm install -g @openai/codex
 
-python -m pip install --upgrade pip
-python -m pip install --upgrade setuptools wheel
-python -m pip install -r requirements.txt
-
 echo "Devcontainer setup complete."
